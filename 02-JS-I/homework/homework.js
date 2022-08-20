4b2b0be3048a6afb877aabc3ccec66fac62ac582
Halixen <luguro@gmail.com>
2022-08-20 17:47:33 +0200

    Otra

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -199,8 +199,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  console.log(str + "!") 
-    return agregarSimboloExclamacion;
+  return console.log(str + "!")
   }
 
 
@@ -208,16 +207,14 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  console.log(nombre + ' ' + apellido) 
-    return combinarNombres;
+  return console.log(nombre + ' ' + apellido) 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  console.log ('Hola' + ' ' + nombre + '!')
-    return obtenerSaludo;
+  return console.log ('Hola' + ' ' + nombre + '!')
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
