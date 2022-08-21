83fc3b717941784cd993b21a14ced3f168dcae4b
Halixen <luguro@gmail.com>
2022-08-21 08:45:51 +0200

    F

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -262,7 +262,7 @@ function esVocal(letra){
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
       return "Es vocal";
    } 
-   if (letra.length !== 1) {gi
+   if (letra.length !== 1) {
     return 'Dato incorrecto';
    } 
     return 'Dato incorrecto';
